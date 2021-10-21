1b61d58d5bbcc895552c25ae57feadc5df1303f1
Sekou Perry <sekou.perry@gmail.com>
2021-10-21 17:13:07 -0400

    update

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ newspapers.forEach(newspaper => {
             const html = response.data
             const $ = cheerio.load(html)
 
-            $('a:contains("crown-jewel")', html).each(function () {
+            $('a:contains("wwe")', html).each(function () {
                 const title = $(this).text()
                 const url = $(this).attr('href')
 
@@ -115,7 +115,7 @@ app.get('/news/:newspaperId', (req, res) => {
             const $ = cheerio.load(html)
             const specificArticles = []
 
-            $('a:contains("crown-jewel")', html).each(function () {
+            $('a:contains("wwe")', html).each(function () {
                 const title = $(this).text()
                 const url = $(this).attr('href')
                 specificArticles.push({
