494b1ffda661d61640790e282294d7e3ce617b78
Sekou Perry <sekou.perry@gmail.com>
2021-10-22 17:11:13 -0400

    a

--- index.js
+++ index.js
@@ -33,8 +33,7 @@ const newspapers = [
     {
         name: 'cbs sports',
         address: 'https://www.cbssports.com/wwe/news/2021-wwe-draft-results-smackdown-and-raw-rosters-picks-all-undrafted-superstars/',
-        base: 'https://www.cbssports.com/wwe/news/2021-wwe-draft-results-smackdown-and-raw-rosters-picks-all-undrafted-superstars/',
-    },
+        base: 'https://www.cbssports.com/',
     {
         name: 'bleacherreport',
         address: 'https://bleacherreport.com/wwe',
